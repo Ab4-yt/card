569ad78c0d1a6f8524746dc2c74d0295dbc23b11
JuIcEPoopooumgood <josifbell@gmail.com>
2024-12-08 21:38:24 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ const elements = {
 	statusImage: document.getElementById("status-image"),
 	displayName: document.querySelector(".display-name"),
 	username: document.querySelector(".username"),
-	customStatus: document.querySelector(".name"),
+	customStatus: document.querySelector(".custom-status"),
 	customStatusText: document.querySelector(".custom-status-text"),
 	customStatusEmoji: document.getElementById("custom-status-emoji"),
 };
